63032644991243137ee6e04cf4a68dbb093e523f
Arne Herbots <arne.herbots@devoteam.com>
2016-09-30 15:59:20 +0200

    Changed the clientID and redirectUri added mail, phone, jobtitle tot user-list

--- app/providers/conf/conf.ts
+++ app/providers/conf/conf.ts
@@ -6,8 +6,8 @@ export class Conf {
   constructor() { throw new Error("Cannot new this class"); }
 
   static authority          = "https://login.windows.net/common";
-  static redirectUri        = "http://MyDirectorySearcherApp";
+  static redirectUri        = "http://DevoteamContactSearcherApp";
   static resourceUri        = "https://graph.windows.net";
-  static clientId           = "a24547d5-1611-48a6-8ba3-9773c9fcab10";
+  static clientId           = "54d04588-2045-45f8-82e1-3d6960340840";
   static graphApiVersion    = "2013-11-08";
 }
